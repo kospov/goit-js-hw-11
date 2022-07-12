7cf40febb77dcd31f21b36332aac5f972d3a62e3
Konstantin Popov <popov.k.g@gmail.com>
2022-07-13 02:41:14 +0300

    qwe

--- src/index.js
+++ src/index.js
@@ -6,12 +6,14 @@ import createPhotoCard from './template/photo-card.hbs'
 const refs = {
     searchFormEl: document.querySelector('#search-form'),
     galleryListEl: document.querySelector('.gallery'),
+    loadMoreBtnEl:document.querySelector('.load-more'),
 };
 
 const pixabayApi = new PixabayApi();
 
-refs.searchFormEl.addEventListener('submit', onSubmitBtnClick);
+// refs.loadMoreBtnEl.classList.add('is-hidden')
 
+refs.searchFormEl.addEventListener('submit', onSubmitBtnClick);
 
 function onSubmitBtnClick (e) {
     e.preventDefault();
@@ -19,13 +21,24 @@ function onSubmitBtnClick (e) {
     const formEl = e.target.elements;
     const queryEl = formEl.searchQuery.value;
 
+    refs.loadMoreBtnEl.classList.toggle('is-hidden');
+
     pixabayApi.fetchPhotos(queryEl)
         .then(data => {
+            if (data.totalHits === 0) {
+                refs.galleryListEl.innerHTML = '';
+                refs.loadMoreBtnEl.classList.add("is-hidden");
 
-            if (data.total === 0) {
-                return showAlertMessage();
+                showAlertMessage();
+                console.log(typeof data.totalHits);
+                return;
+            } else {
+                refs.galleryListEl.innerHTML = createPhotoCard(data.hits);
+                console.log(data.totalHits);
+
+                refs.loadMoreBtnEl.classList.remove("is-hidden");
             }
 
-            refs.galleryListEl.innerHTML = createPhotoCard(data.hits);
+            
         });
 }
